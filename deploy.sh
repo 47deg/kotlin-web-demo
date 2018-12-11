7eddb68f50fc94d2a60a696c24f608148e2c5228
domlopez <domlopez@starbucks.com>
2018-12-11 20:39:08 +0100

    Cleaning up

--- deploy.sh
+++ deploy.sh
@@ -4,36 +4,36 @@ rm -rf try.arrow-kt.web
 
 git clone https://github.com/JetBrains/kotlin-web-demo try.arrow-kt.web
 
-sudo cp deploy/docker-compose.yml ./try.arrow-kt.web
+sudo cp deploy/docker-compose.yml try.arrow-kt.web
 
-sudo cp arrow/arrowKtVersion ./try.arrow-kt.web
+sudo cp arrow/arrowKtVersion try.arrow-kt.web
 
-sudo cp deploy/.secret ./try.arrow-kt.web
+sudo cp deploy/.secret try.arrow-kt.web
 
-sudo cp ./cert/* ./try.arrow-kt.web/docker/frontend/conf
+sudo cp cert/* try.arrow-kt.web/docker/frontend/conf
 
-sudo cp server.xml ./try.arrow-kt.web/docker/frontend/conf
+sudo cp server.xml try.arrow-kt.web/docker/frontend/conf
 
-for i in ./try.arrow-kt.web/versions/*/build.gradle
+for i in try.arrow-kt.web/versions/*/build.gradle
 do
    awk 'FNR==1{print ""}1' arrow/arrow-dependencies >> $i
 done
 
-awk 'FNR==1{print ""}1' arrow/arrow-repositories >> ./try.arrow-kt.web/build.gradle
+awk 'FNR==1{print ""}1' arrow/arrow-repositories >> try.arrow-kt.web/build.gradle
 
-awk 'FNR==1{print ""}1' deploy/web-demo-backend >> ./try.arrow-kt.web/docker/backend/Dockerfile
+awk 'FNR==1{print ""}1' deploy/web-demo-backend >> try.arrow-kt.web/docker/backend/Dockerfile
 
-awk 'FNR==1{print ""}1' deploy/web-demo-war >> ./try.arrow-kt.web/docker/frontend/Dockerfile
+awk 'FNR==1{print ""}1' deploy/web-demo-war >> try.arrow-kt.web/docker/frontend/Dockerfile
 
-awk 'FNR==1{print ""}1' arrow/arrow-executors-policy >> ./try.arrow-kt.web/kotlin.web.demo.backend/src/main/resources/executors.policy.template
+awk 'FNR==1{print ""}1' arrow/arrow-executors-policy >> try.arrow-kt.web/kotlin.web.demo.backend/src/main/resources/executors.policy.template
 
 cd try.arrow-kt.web
 
 sh gradlew
 
-mkdir ./docker/frontend/war/
+mkdir docker/frontend/war/
 
-mkdir ./docker/backend/war/
+mkdir docker/backend/war/
 
 export ARROW_VERSION=$(cat arrow/arrowKtVersion)
 
@@ -43,9 +43,9 @@ sh gradlew clean
 
 sh gradlew war
 
-sudo mv ./kotlin.web.demo.server/build/libs/WebDemoWar.war ./docker/frontend/war/WebDemoWar.war
+sudo mv kotlin.web.demo.server/build/libs/WebDemoWar.war docker/frontend/war/WebDemoWar.war
 
-sudo mv ./kotlin.web.demo.backend/build/libs/WebDemoBackend.war ./docker/backend/war/WebDemoBackend.war
+sudo mv kotlin.web.demo.backend/build/libs/WebDemoBackend.war docker/backend/war/WebDemoBackend.war
 
 sudo docker-compose down
 
