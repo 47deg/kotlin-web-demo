47f4440547eb44c6b9156b654e6fe5cdad037720
domlopez <domlopez@starbucks.com>
2019-02-12 17:33:26 +0100

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ sudo cp cert/* -r try.arrow-kt.web/docker/frontend/conf
 
 sudo cp server.xml try.arrow-kt.web/docker/frontend/conf
 
-curl -s https://api.bintray.com/search/packages\?name\=arrow\&repo\=arrow-kt |
+curl -s https://api.bintray.com/search/packages?name=arrow&repo=arrow-kt |
    python -c "import json,sys;obj=json.load(sys.stdin);print '\n'.join(obj[0]['system_ids']);" |
    awk 'gsub("/", "");' > arrow/arrow-full-dependencies
    awk 'NR==FNR{a[$0];next} !($0 in a)' arrow/arrow-deprecated-dependencies arrow/arrow-full-dependencies |
